577e1a6dd8e2380f39ca6a74a6ba7db4f8b9a377
bluelemon61 <67902252+bluelemon61@users.noreply.github.com>
2024-12-28 18:33:15 +0900

    chore: lint 설정 및 webpack 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import ESLintPlugin from 'eslint-webpack-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    config.plugins.push(
+      new ESLintPlugin({
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        emitWarning: true,
+      }),
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
