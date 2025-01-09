1e1fca7de929b585d5273c5202eee05cb2b9280c
mangsgi <aodrntjr0901@naver.com>
2025-01-09 20:02:45 +0900

    fix: strengthen authentication and authorization

--- app/api/team/route.ts
+++ app/api/team/route.ts
@@ -6,16 +6,29 @@ import { auth } from '@/libs/auth';
 // Create Team
 export async function POST(request: NextRequest) {
   try {
+    const session = await auth();
     const body = await request.json();
     const { login_id, password } = body;
 
     const hashedPw = await hashingPassword(password);
+
+    if (session?.user.role !== 'ADMIN') {
+      return NextResponse.json(
+        { error: 'ADMIN만 TEAM 생성이 가능합니다.' },
+        { status: 401 },
+      );
+    }
 
     const user = await prisma.user.create({
       data: {
         login_id,
         password: hashedPw,
         role: 'TEAM',
+      },
+      select: {
+        login_id: true,
+        role: true,
+        id: true,
       },
     });
 
@@ -50,6 +63,9 @@ export async function GET() {
     // 로그인한 유저가 관리자일 때
     if (session?.user.role === 'ADMIN') {
       const users = await prisma.user.findMany({
+        where: {
+          role: 'TEAM',
+        },
         select: {
           id: true,
           login_id: true,
@@ -79,8 +95,26 @@ export async function GET() {
 // Delete Team
 export async function DELETE(request: NextRequest) {
   try {
+    const session = await auth();
     const params = request.nextUrl.searchParams;
     const userId = params.get('id');
+
+    // 로그인되지 않았을 때
+    if (!session?.user) {
+      return NextResponse.json(
+        {
+          message: '로그인이 필요합니다.',
+        },
+        { status: 401 },
+      );
+    }
+
+    if (session?.user.role !== 'ADMIN') {
+      return NextResponse.json(
+        { error: '관리자만 삭제가 가능합니다.' },
+        { status: 401 },
+      );
+    }
 
     if (!userId) {
       return NextResponse.json(
@@ -111,11 +145,29 @@ export async function PUT(request: NextRequest) {
   try {
     const body = await request.json();
     const { id, login_id, newPassword } = body;
+    const session = await auth();
 
     if (!id) {
       return NextResponse.json(
         { error: 'User ID is required' },
         { status: 400 },
+      );
+    }
+
+    // 로그인되지 않았을 때
+    if (!session?.user) {
+      return NextResponse.json(
+        {
+          message: '로그인이 필요합니다.',
+        },
+        { status: 401 },
+      );
+    }
+
+    if (session?.user.role !== 'ADMIN') {
+      return NextResponse.json(
+        { error: '관리자만 변경할 수 있습니다.' },
+        { status: 401 },
       );
     }
 
