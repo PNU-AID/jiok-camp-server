900b08c360ec7358b143a28c8dfc7edd9e9b08e9
bluelemon61 <67902252+bluelemon61@users.noreply.github.com>
2025-01-16 00:48:04 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,11 @@ module.exports = {
       exec_mode: 'cluster', // 실행 모드: cluster 또는 fork 중 선택
       instances: '2', // 클러스터 모드에서 실행할 인스턴스 수 (CPU 코어 수만큼)
       autorestart: true, // 프로세스 자동 재시작 활성화
-      watch: true, // 파일 변경 감지 활성화 (개발 중에만 활용)
+      watch: false, // 파일 변경 감지 활성화 (개발 중에만 활용)
       max_memory_restart: '1G', // 1GB 이상 메모리 사용 시 재시작
       env: {
         NODE_ENV: 'production', // Node.js 환경 설정
       },
-      ignore_watch: ['csv'],
     },
   ],
 };
