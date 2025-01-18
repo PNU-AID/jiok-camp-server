7c48c8a45f81c4149dfdab0a976cbfb18ff1469c
bluelemon61 <67902252+bluelemon61@users.noreply.github.com>
2025-01-18 22:50:41 +0900

    fix: 노션 페이지 링크 수정

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 export const ScoreOpenDate = new Date(2025, 0, 24);
 
 export const downloadUrl =
-  'https://aidpnu.notion.site/AID-AI-Developer-11ef0e0d194f8108951efc1bce4b42fc?pvs=74';
+  'https://aidpnu.notion.site/AID-2-179f0e0d194f804b9d88d723427c46b1';
 
 export const aidUrl = {
   homepage: 'https://pnu-aid.github.io/homepage/',
